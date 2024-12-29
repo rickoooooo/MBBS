9b5912929baab9b46ffbdaadacc82c583151d03f
Rick <rick@richardosgood.com>
2024-12-28 21:28:20 -0800

    Added watchdog thread in Meshtastic TCP interface to watch for broken connections and restart the interface. This can happen if another device connects to the meshtastic radio as the radio will only allow one TCP connection at a time.

--- interfaces/comm_interface_meshtastic_tcp.py
+++ interfaces/comm_interface_meshtastic_tcp.py
@@ -2,7 +2,9 @@ from utils.log import logger
 from interfaces.comm_interface import CommInterface
 from pubsub import pub
 import meshtastic.tcp_interface
+import threading
 import time
+import socket
 
 '''
 This interface allows the BBS to communicate over a Meshtastic radio device by using TCP to connect to the device.
@@ -13,6 +15,9 @@ class CommInterfaceMeshtasticTCP(CommInterface):
         self.radio_ip = radio_ip
         self.channel_index = channel_index
         self.interface = meshtastic.tcp_interface.TCPInterface(self.radio_ip)
+
+        self.monitor_thread = threading.Thread(target=self.check_socket_closed)
+        self.monitor_thread.start()
 
         # Meshtastic uses pubsub to receive messages from the radios asynchronously
         pub.subscribe(self.on_receive, "meshtastic.receive")
@@ -43,3 +48,33 @@ class CommInterfaceMeshtasticTCP(CommInterface):
     '''
     def close(self) -> None:
         self.interface.close()
+
+    '''
+    Hack to test if the TCP socket is still open
+    Meshtastic radio only allows one connection at a time. If anything else touches the port, it will disconnect the BBS.
+    The Meshtastic API doesn't currently have a way to monitor and handle this condition, so we test here and reset as needed.
+    '''
+    def check_socket_closed(self) -> bool:
+        while True:
+            sock = self.interface.socket
+            try:
+                # this will try to read bytes without blocking and also without removing them from buffer (peek only)
+                data = sock.recv(16, socket.MSG_DONTWAIT | socket.MSG_PEEK)
+                if len(data) == 0:
+                    self.socket_reset()
+            except BlockingIOError:
+                pass  # socket is open and reading from it would block
+            except ConnectionResetError:
+                self.socket_reset()  # socket was closed for some other reason
+            except Exception as e:
+                logger.error(str(e))
+                pass
+            time.sleep(1)
+
+    '''
+    Resets the TCP interface if it was disconnected for some reason
+    '''
+    def socket_reset(self) -> None:
+        self.interface.close()
+        self.interface = meshtastic.tcp_interface.TCPInterface(self.radio_ip)
+
